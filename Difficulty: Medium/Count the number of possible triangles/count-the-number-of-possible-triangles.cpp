b6e6839cf965486c9a65e0279d274b834db6c831
KAMPARAPU LAKSHMAN <119840360+lakshmank6300@users.noreply.github.com>
2025-01-08 22:10:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the number of possible triangles.
+    int countTriangles(vector<int>& arr) {
+        // code here
+        int n = arr.size(); 
+        sort(arr.begin(),arr.end());
+        int count = 0; 
+        for (int i = 0; i < n - 2; ++i) { 
+            int k = i + 2; 
+            for (int j = i + 1; j < n; ++j) 
+            { 
+                while (k < n && arr[i] + arr[j] > arr[k]) 
+                {
+                    ++k; 
+                }
+                
+                if (k > j) 
+                    count += k - j - 1; 
+            } 
+        } 
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.countTriangles(a) << "\n~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
